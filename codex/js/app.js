1b79fc1cb19daa5a5e0c243bc77304bc12fe3d86
AleksandrLiebert <aleksandr.liebert@gmail.com>
2019-10-01 19:43:17 +0700

    Новая навигация

--- codex/js/app.js
+++ codex/js/app.js
@@ -1,7 +1,116 @@
-function resizeWindow() {
-  $("iframe").attr('height', $("iframe")[0].offsetWidth / 16 * 9)
-}
+ function print(data) {
+   console.log(data)
+ }
 
-$(window).resize(resizeWindow)
+  function setMain() {
+    $('#sigil').html('<img src="img/dd.png" width="100%">')
+    $('#title').text(content.main.name)
+    $('#content').html(content.main.content)
+  }
 
-resizeWindow()
+  function setCategory(id){
+    var category
+    for (var i = 0; i < content.categories.length; i++){
+      if (content.categories[i].id == id) {
+        category = content.categories[i]
+        break
+      }
+    }
+
+    $.get('content/' + id + '.html', function(data) {
+      $('#sigil').html('<img src="img/' + category.id + '.png" width="100%">')
+      $('#title').text(category.name)
+      $('#content').html(data)
+
+      $('.cross').click(function() {
+        var id = this.getAttribute('data')
+        showCategory(id)
+        return false
+      })
+      resizeWindow()
+    })
+  }
+
+  function showCategory(id){
+    history.pushState(null, null, '?c=' + id)
+    reload()
+  }
+
+  function showContent() {
+    history.pushState(null, null, '?c=content')
+    reload()
+  }
+
+  function reload() {
+    print('reload')
+    var url = new URL(window.location.href)
+    var c = url.searchParams.get("c");
+    if (category.style.display == 'none' && contents.style.display == 'none') {
+      if (c == null) {
+        setMain()
+        $('#category').fadeIn(400)
+      } else if (c == 'content') {
+        $('#contents').fadeIn(400)
+      } else {
+        setCategory(c)
+        $('#category').fadeIn(400)
+        //setTimeout(resizeWindow, 500)
+      }
+    } else {
+      $('#category').fadeOut(400)
+      $('#contents').fadeOut(400)
+
+      if (c == null) {
+        setMain()
+        $('#category').delay(400).fadeIn(400)
+      } else if (c == 'content') {
+        $('#contents').delay(400).fadeIn(400)
+      } else {
+        setTimeout(() => setCategory(c), 400)
+        $('#category').delay(400).fadeIn(400)
+        //setTimeout(resizeWindow, 500)
+      }
+    }
+  }
+
+  $.getJSON('content.json', function(data){
+    content = data
+    var res = '<div class="row">'
+    for (var i = 0; i < data.categories.length; i++) {
+      res += '<div class="col-md-3 col-6">'
+      res += '<a class="c-img" data="' + data.categories[i].id + '" href="?c=' + data.categories[i].id + '"><img src="img/' + data.categories[i].id  + '.png" width="100%" style="cursor: pointer;"></a>'
+      res += '<p>' + data.categories[i].name + '</p>'
+      res += '</div>'
+    }
+    res += '</div>'
+    $('#contents').html(res)
+
+    $('.c-img').click(function() {
+      var id = this.getAttribute('data')
+      showCategory(id)
+      return false
+    })
+
+    window.onpopstate = reload
+    reload()
+  }).fail(function() {
+    console.log('fail')
+  })
+
+  $('.content').click(function() {
+    showContent()
+    return false
+  })
+
+  function resizeWindow() {
+    if ($('iframe').length == 0) {
+      return
+    }
+    if ($("iframe")[0].offsetWidth == 0) {
+      setTimeout(resizeWindow, 200)
+    } else {
+      $("iframe").attr('height', $("iframe")[0].offsetWidth / 16 * 9)
+    }
+  }
+
+  $(window).resize(resizeWindow)
